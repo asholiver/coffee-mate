adcd36f513ca1d4d4b3cf78f5790c9ae97318d5d
Ashley Oliver <ashleyoliver@192-168-1-101.tpgi.com.au>
2019-03-05 21:55:08 +0000

    container modifiers

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./Container.css";
 import classNames from "classnames";
 
-const Container = ({ size, children }) => {
-    const classes = classNames({
-        "l-container": true,
-        [`l-container--${size}`]: size == null ? false : true
-    });
-    return <div className={classes}>{children}</div>;
+const Container = ({ size, children, modifiers }) => {
+  const classes = classNames({
+    "l-container": true,
+    [`l-container--${size}`]: size == null ? false : true,
+    [`${modifiers}`]: modifiers == null ? false : true
+  });
+  return <div className={classes}>{children}</div>;
 };
 
 export default Container;
