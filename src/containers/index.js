115f673496a6cc2739e29748ecbf7472e61493e0
Ashley Oliver <ashleyoliver@192-168-1-101.tpgi.com.au>
2019-03-05 21:51:44 +0000

    new containers

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,23 +1,27 @@
+import CreateAccount from "./CreateAccount";
 import CreateMember from "./CreateMember";
 import CreateGroup from "./CreateGroup";
 import Favourites from "./Favourites";
 import GroupChannel from "./GroupChannel";
 import GroupDetails from "./GroupDetails";
 import Groups from "./Groups";
-import LandingPage from "./LandingPage";
+import Login from "./Login";
+import ResetPassword from "./ResetPassword";
 import Rewards from "./Rewards";
 import Search from "./Search";
 import Settings from "./Settings";
 
 export {
-    CreateMember,
-    CreateGroup,
-    Favourites,
-    GroupChannel,
-    GroupDetails,
-    Groups,
-    LandingPage,
-    Rewards,
-    Search,
-    Settings
+  CreateAccount,
+  CreateMember,
+  CreateGroup,
+  Favourites,
+  GroupChannel,
+  GroupDetails,
+  Groups,
+  Login,
+  ResetPassword,
+  Rewards,
+  Search,
+  Settings
 };
