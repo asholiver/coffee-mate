0ad6316a842a6189b1130063301eb4f9715e8873
Ashley Oliver <ashleyoliver@Ashleys-MacBook-Pro.local>
2019-03-12 06:58:14 +0000

    update to use react hooks

--- src/containers/CreateAccount/CreateAccount.js
+++ src/containers/CreateAccount/CreateAccount.js
@@ -1,54 +1,99 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { TextField, ButtonGroup } from "../../components";
+import { Form, TextField, ButtonGroup } from "../../components";
 import { LoggedOut } from "../../layout";
 
-class CreateAccount extends Component {
-  state = {
-    is_loading: true,
-    names: [],
-    new_username: "",
-    isSideBarVisible: false
-  };
-  handleChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
+const CreateAccount = () => {
+  const [values, setValues] = useState({
+    new_first: "",
+    new_last: "",
+    new_email: "",
+    new_password: "",
+    new_repeat_password: ""
+  });
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value
     });
   };
-  render() {
-    return (
-      <LoggedOut>
-        <TextField
-          label="Choose username"
-          name="new_username"
-          onChange={this.handleChange}
-        />
-        <TextField
-          label="Create password"
-          name="new_password"
-          isInvalid="true"
-          type="password"
-          onChange={this.handleChange}
-          errorMessage="The password you have entered is incorrect"
-        />
 
-        <ButtonGroup type="space-between">
-          <Link
-            className="c-button c-button--primary c-button--full-width"
-            to="/"
-          >
-            Return to login
-          </Link>
-          <Link
-            className="c-button c-button--secondary c-button--full-width"
-            to="/"
-          >
-            Create
-          </Link>
-        </ButtonGroup>
-      </LoggedOut>
-    );
-  }
-}
+  const loginErrorMessages = {
+    100: "No account associated with this email.",
+    200: "New user - no password",
+    300: "Password is incorrect."
+  };
+
+  const [errorText, setErrorText] = useState("");
+
+  return (
+    <LoggedOut>
+      <TextField
+        label="First name"
+        name="new_first"
+        onChange={handleChange}
+        error={
+          loginErrorMessages[errorText.new_first && errorText.new_first.type]
+        }
+      />
+      <TextField
+        label="First name"
+        name="new_last"
+        onChange={handleChange}
+        error={
+          loginErrorMessages[errorText.new_last && errorText.new_last.type]
+        }
+      />
+      <TextField
+        label="Username"
+        name="new_email"
+        onChange={handleChange}
+        error={
+          loginErrorMessages[errorText.new_email && errorText.new_email.type]
+        }
+      />
+      <TextField
+        label="Create password"
+        name="new_password"
+        type="password"
+        onChange={handleChange}
+        error={
+          loginErrorMessages[
+            errorText.new_password && errorText.new_password.type
+          ]
+        }
+      />
+      <TextField
+        label="Repeat password"
+        name="new_repeat_password"
+        isInvalid="true"
+        type="password"
+        onChange={handleChange}
+        error={
+          loginErrorMessages[
+            errorText.new_repeat_password && errorText.new_repeat_password.type
+          ]
+        }
+      />
+
+      <ButtonGroup type="space-between">
+        <Link
+          className="c-button c-button--primary c-button--full-width"
+          to="/"
+        >
+          Return to login
+        </Link>
+        <Link
+          className="c-button c-button--secondary c-button--full-width"
+          to="/"
+        >
+          Create
+        </Link>
+      </ButtonGroup>
+    </LoggedOut>
+  );
+};
 
 export default CreateAccount;
