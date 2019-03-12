613f3a3111ed2eddc9e16e45d438815773706e77
Ashley Oliver <ashleyoliver@Ashleys-MacBook-Pro.local>
2019-03-12 06:39:20 +0000

    form component

--- src/components/index.js
+++ src/components/index.js
@@ -1,24 +1,28 @@
 import { Button, ButtonGroup, ButtonIconOnly } from "./buttons";
 import Container from "./Container";
+import Form from "./Form";
 import { Select, TextField } from "./FormFields";
 import { Icon, Icons } from "./icons";
 import Link from "./Link";
 import GroupMember from "./GroupMember";
 import Group from "./Group";
 import { NavItem, NavList } from "./navigation";
+import Spacing from "./Spacing";
 
 export {
-    Button,
-    ButtonGroup,
-    ButtonIconOnly,
-    Container,
-    Icon,
-    Icons,
-    Link,
-    GroupMember,
-    Group,
-    NavItem,
-    NavList,
-    Select,
-    TextField
+  Button,
+  ButtonGroup,
+  ButtonIconOnly,
+  Container,
+  Form,
+  Icon,
+  Icons,
+  Link,
+  GroupMember,
+  Group,
+  NavItem,
+  NavList,
+  Select,
+  Spacing,
+  TextField
 };
