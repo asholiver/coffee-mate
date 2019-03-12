26aea8e1a3a8233c853b622f72b05b648d8f3e90
Ashley Oliver <ashleyoliver@Ashleys-MacBook-Pro.local>
2019-03-12 08:22:00 +0000

    use main element

--- src/layout/LoggedOut/LoggedOut.js
+++ src/layout/LoggedOut/LoggedOut.js
@@ -4,12 +4,12 @@ import { Container, Icon } from "../../components";
 const LoggedOut = ({ children }) => {
   return (
     <Container modifiers="l-container--fixed-width">
-      <div className="c-home-page">
+      <main className="c-home-page">
         <div className="c-logo-container h-spacing-2x-large">
           <Icon icon="app-logo" size="large" classes="c-logo" />
         </div>
         {children}
-      </div>
+      </main>
     </Container>
   );
 };
